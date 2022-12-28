5ba4cff768de79782e81eb9c5dad0acd9b905313
Oleksandra Kryvobok <88559507+OleksandraKryvobok@users.noreply.github.com>
2022-12-28 23:25:56 +0200

    add changes before check

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(({ seconds }) => {
+    console.log(seconds);
+    localStorage.setItem(CURRENT_TIME, JSON.stringify(seconds));
+}, 1000));
+
+player.setCurrentTime(Number(localStorage.getItem(CURRENT_TIME)));
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+};
+
+const formData = {};
+const STORAGE_KEY = "feedback-form-state";
+
+refs.form.addEventListener('input', throttle(onFormChange, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+populateFormData();
+
+function onFormChange(e) {
+    formData.email = e.currentTarget.elements.email.value;
+    formData.message = e.currentTarget.elements.message.value;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    e.currentTarget.reset();
+
+    localStorage.removeItem(STORAGE_KEY);
+
+    console.log(formData);
+}
+
+function populateFormData() {
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if(savedData) {
+        refs.input.value = savedData.email;
+        refs.textarea.value = savedData.message;
+
+
+        console.log(savedData);
+    }
+
+}
+
 
